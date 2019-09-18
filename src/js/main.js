0fa60b9436fdbc90d74516b3b9c6546b489e3f18
Arun Kumar <arunkumar7617@gmail.com>
2019-09-19 00:03:35 +0400

    final commit -  add testimonial, contact and footer section

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-(function global() {
+(function global($) {
   function menuToggle() {
     const navs = document.querySelectorAll('.navbar-items-right');
 
@@ -8,4 +8,29 @@
   document
     .querySelector('.navbar-menu-button')
     .addEventListener('click', menuToggle);
-})();
+
+  $(document).ready(function($) {
+    'use strict';
+    $('#client-testimonials').owlCarousel({
+      loop: true,
+      center: true,
+      items: 3,
+      margin: 0,
+      autoplay: true,
+      dots: true,
+      autoplayTimeout: 6000,
+      smartSpeed: 400,
+      responsive: {
+        0: {
+          items: 1
+        },
+        768: {
+          items: 2
+        },
+        1170: {
+          items: 3
+        }
+      }
+    });
+  });
+})(window.jQuery);
